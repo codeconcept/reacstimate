2df3eea225e3c20c62410b928ebf0cfb8d0e85fe
codeconcept <contact@codeconcept.fr>
2018-09-03 20:35:55 +0200

    editing first component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h2>Bienvenue sur votre générateur de devis</h2>
+        <div>
+          Créer un devis ? C'est simple : cliquer sur <button>nouveau devis</button>
+        </div>
       </div>
     );
   }
